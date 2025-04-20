64d3642078741ccd9f6f9417fa04bd87ab995cd9
hndrr <yozhik20@gmail.com>
2025-04-20 12:59:36 +0900

    App.tsxからReactのインポートを修正し、不要なImageUploaderのインポートをコメントアウトしました。

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useAppStore } from "./store/useAppStore";
 import Header from "./components/Header";
 // import ImageUploader from "./components/ImageUploader"; // 削除
